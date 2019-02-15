4bc81c9294adccb7781c95f1c35ffc65227753c6
c0ndensate <bobbyconnolly@gmail.com>
2019-02-15 16:17:13 -0500

    improvements

--- backend/routes/claim/claim-add.js
+++ backend/routes/claim/claim-add.js
@@ -1,17 +1,38 @@
 const router = require("express").Router()
 const getDb = require(global.appRoot + "/db").getDb
 
+// request = {
+//   licence: "123456",
+//   location: "12345",
+//   medicare: "XXXX01010112",
+//   dx: "12345",
+//   billingLines: [
+//     { lineNumber: "1", serviceDate: "19-02-15", billingCode: "12345" },
+//     { lineNumber: "2", serviceDate: "19-02-15", billingCode: "54321" }
+//   ]
+// }
+
+// response = {
+//   success: true,
+//   chitNumber: 123
+// }
+//
+// response = {
+//   success: false,
+//   error: "Incomplete claim"
+// }
+
 router.post("/", async (req, res) => {
   try {
     let claim = JSON.parse(req.body)
     const { licence, location, medicare, dx, billingLines } = claim
 
     if (!licence || !location || !medicare || !dx || !billingLines) {
-      return res.status(400).json({ error: "Incomplete claim" })
+      throw new Error("Incomplete claim")
     }
 
     if (billingLines.constructor !== Array) {
-      return res.status(400).json({ error: "Billing lines not an array" })
+      throw new Error("Billing lines not an array")
     }
 
     if (
@@ -20,7 +41,7 @@ router.post("/", async (req, res) => {
         return lineNumber && serviceDate && billingCode
       })
     ) {
-      return res.status(400).json({ error: "Incomplete billing line" })
+      throw new Error("Incomplete billing line")
     }
 
     if (
@@ -28,14 +49,12 @@ router.post("/", async (req, res) => {
         return elem.lineNumber == index + 1 // Don't use === since lineNumber is a string
       })
     ) {
-      return res.status(400).json({
-        error: "Billing line numbers not sequential or not starting at 1"
-      })
+      throw new Error("Billing line numbers not ordered from 1")
     }
 
     const db = getDb()
 
-    claim.chitNumber = (await db
+    const chitNumber = (await db
       .collection("counters")
       .findOneAndUpdate(
         { _id: "chitNumber" },
@@ -43,11 +62,13 @@ router.post("/", async (req, res) => {
         { returnOriginal: false }
       )).value.sequence_value
 
+    claim.chitNumber = chitNumber
+
     await db.collection("claims").insertOne(claim)
 
-    return res.status(202).send("Claim added")
+    return res.status(202).json({ success: true, chitNumber })
   } catch (error) {
-    return res.status(400).send("Invalid claim: " + error)
+    return res.status(400).json({ success: false, error })
   }
 })
 

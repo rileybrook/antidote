43cc8b9a5c785a21392250023fc9ddf261ba4721
c0ndensate <bobbyconnolly@gmail.com>
2019-02-18 18:21:23 -0500

    changes made to riley folder

---
+++ workbench/riley/src/MyComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+class MyComponent extends Component {
+  constructor() {
+    super()
+
+    this.state = {}
+  }
+
+  MyFunction = () => {}
+
+  render() {
+    return <div />
+  }
+}
+
+export default MyComponent

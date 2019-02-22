5947ca2374079a9fd39ff5286b2f157ba94d3d6d
rileybrook <rileyabrook@gmail.com>
2019-02-21 19:38:02 -0500

    AppNav.js icons & highlighting added

--- frontend/src/components/AppNav.js
+++ frontend/src/components/AppNav.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react"
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
+import SideNav, {
+  Toggle,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from "reactstrap"
+  NavIcon,
+  NavText
+} from "@trendmicro/react-sidenav"
+import "@trendmicro/react-sidenav/dist/react-sidenav.css"
 class AppNav extends Component {
   state = {
     isOpen: false
@@ -23,20 +18,62 @@ class AppNav extends Component {
   }
   render() {
     return (
-      <header className="color-black side-nav">
-        <div>
-          <Nav vertical className="color-white">
-            <NavItem>Logo</NavItem>
-            <NavItem>Search</NavItem>
-            <NavItem>3rd Item</NavItem>
-            {/* <Navbar light expand="md">
-              <NavbarBrand className="color-white" href="/">
-                Antidote
-              </NavbarBrand>
-            </Navbar> */}
-          </Nav>
-        </div>
-      </header>
+      <SideNav className="color-black ">
+        <Nav defaultSelected="home">
+          <NavItem eventKey="home">
+            <NavIcon>
+              <i className="fa fa-fw fa-home" style={{ fontSize: "2em" }} />
+            </NavIcon>
+          </NavItem>
+          <NavItem eventKey="search">
+            <NavIcon>
+              <i
+                className="fa fa-fw fa-search-plus"
+                style={{ fontSize: "2em" }}
+              />
+            </NavIcon>
+          </NavItem>
+          <NavItem eventKey="card">
+            <NavIcon>
+              <i className="fa fa-fw fa-id-card" style={{ fontSize: "2em" }} />
+            </NavIcon>
+          </NavItem>
+          <NavItem eventKey="file">
+            <NavIcon>
+              <i className="fa fa-fw fa-file" style={{ fontSize: "2em" }} />
+            </NavIcon>
+          </NavItem>
+          <NavItem eventKey="history">
+            <NavIcon>
+              <i className="fa fa-fw fa-history" style={{ fontSize: "2em" }} />
+            </NavIcon>
+          </NavItem>
+          <NavItem eventKey="settings">
+            <NavIcon>
+              <i className="fa fa-fw fa-cog" style={{ fontSize: "2em" }} />
+            </NavIcon>
+          </NavItem>
+          <NavItem eventKey="github">
+            <NavIcon>
+              <i className="fa fa-fw fa-github" style={{ fontSize: "2em" }} />
+            </NavIcon>
+          </NavItem>
+        </Nav>
+      </SideNav>
+      // <header className="color-black side-nav">
+      //   <div>
+      //     <Nav vertical className="color-white" defaultSelected="home">
+      //       <NavItem eventKey="home">Logo</NavItem>
+      //       <NavItem>Search</NavItem>
+      //       <NavItem>3rd Item</NavItem>
+      //       {/* <Navbar light expand="md">
+      //         <NavbarBrand className="color-white" href="/">
+      //           Antidote
+      //         </NavbarBrand>
+      //       </Navbar> */}
+      //     </Nav>
+      //   </div>
+      // </header>
     )
   }
 }
